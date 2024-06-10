7b3fc5c3eebe0b98db1c758afb79fe5fb7b6716c
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-06-10 21:55:12 +0500

    Delete tests/test_data.py

--- tests/test_data.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import torchvision
-
-def test_data_loading():
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False)
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False)
-    assert len(trainset) == 50000
-    assert len(testset) == 10000
