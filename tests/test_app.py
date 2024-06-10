751bee943dc02b08b0c65755323d8fcce49f0835
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-06-11 00:48:02 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,17 @@
 import unittest
 import requests
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import predict
 
 class TestApp(unittest.TestCase):
-    def test_predict(self):
-        # Проверка функции predict()
-        # Подготавливаем изображение, используя PIL
-        image = prepare_image()
-        predicted_class = predict(image)
-        # Проверяем, что возвращенный класс находится в списке классов
-        self.assertIn(predicted_class, classes)
-
-    def test_app_availability(self):
-        # Проверка доступности приложения по ссылке
-        url = 'http://localhost:8501'  # Замените на вашу ссылку
-        response = requests.get(url)
+    def test_homepage(self):
+        response = requests.get("http://localhost:8501")
         self.assertEqual(response.status_code, 200)
+
+    def test_prediction(self):
+        url = "http://localhost:8501/predict"
+        files = {'file': open('tests/sample_image.png', 'rb')}
+        response = requests.post(url, files=files)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("prediction", response.json())
 
 if __name__ == '__main__':
     unittest.main()
