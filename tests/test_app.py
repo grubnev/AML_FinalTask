64aaa0baaeb6403088a8ce9af884e0fd55372100
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-06-11 13:36:49 +0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,10 +5,3 @@ class TestApp(unittest.TestCase):
     def test_homepage(self):
         response = requests.get("http://localhost:8501")
         self.assertEqual(response.status_code, 200)
-
-    def test_prediction(self):
-        url = "http://localhost:8501/predict"
-        files = {'file': open('tests/sample_image.png', 'rb')}
-        response = requests.post(url, files=files)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("prediction", response.json())
