57a77677273c86a9bb4a7993d603d2bcf5966173
Nikita Grebnev <81284413+grubnev@users.noreply.github.com>
2024-06-11 13:37:30 +0500

    Update test_data_quality.py

--- tests/test_data_quality.py
+++ tests/test_data_quality.py
@@ -1,12 +1,12 @@
 import pytest
 import pandas as pd
 
-CIFAR10_DATA_PATH = "data/cifar-10-python.tar.gz.dvc"
+CIFAR100_DATA_PATH = "data/cifar-100-python.tar.gz.dvc"
 
 @pytest.fixture
-def cifar10_data():
-    data = pd.read_csv(CIFAR10_DATA_PATH)
+def cifar100_data():
+    data = pd.read_csv(CIFAR100_DATA_PATH)
     return data
 
-def test_data_not_empty(cifar10_data):
-    assert not cifar10_data.empty
+def test_data_not_empty(cifar100_data):
+    assert not cifar100_data.empty
